64ceed5211991a68593127da0f9b890742334808
Enti-Pol <pol.garriga@enti.cat>
2021-03-16 20:54:07 +0100

    Torreta de agua

---
+++ Assets/Scripts/bulletController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    public float baseSpeed = 10f;
+    public GameObject ship;
+    public int damage = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = -transform.right * baseSpeed;
+        ship = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
+        {
+            collision.gameObject.GetComponent<playerController>().hp = collision.gameObject.GetComponent<playerController>().hp - damage;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/turretController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turretController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    private float timeToShoot = 0;
+    public float fireRate = 2000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float delta = Time.deltaTime * 1000;
+        bool canShoot = false;
+        timeToShoot += delta;
+        if (timeToShoot > fireRate)
+        {
+            canShoot = true;
+            timeToShoot = 0;
+        }
+
+        if (canShoot)
+        {
+            timeToShoot = 0;
+            GameObject bullet = Instantiate(bulletPrefab, new Vector3(transform.position.x - 1, transform.position.y, 1), transform.rotation);
+            Destroy(bullet, 3);
+        }
+    }
+}
