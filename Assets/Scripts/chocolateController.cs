4143b7810b6839800895e12929f4e1d725f0e1f6
Enti-Pol <pol.garriga@enti.cat>
2021-03-16 21:56:17 +0100

    Chocolate caliente + final tutorial

---
+++ Assets/Scripts/chocolateController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chocolateController : MonoBehaviour
+{
+    public bool playerDetect = false;
+
+    private Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(playerDetect);
+        float delta = Time.deltaTime * 1000;
+        if (playerDetect)
+        {
+            rigidBody.velocity = new Vector2(0, 0.3f) * delta;
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0, 0) * delta;
+        }
+    }
+}
---
+++ Assets/Scripts/detectPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2")
+        {
+            gameObject.transform.parent.GetComponent<chocolateController>().playerDetect = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        gameObject.transform.parent.GetComponent<chocolateController>().playerDetect = false;
+    }
+}
