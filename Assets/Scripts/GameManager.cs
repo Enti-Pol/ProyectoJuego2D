d20c414ca96836faa92e499d0ef3dec4b9b73848
Enti-Pol <pol.garriga@enti.cat>
2021-04-02 00:57:21 +0200

    Cambios en el sistema de Sonido
    Preparado para poder añadir sonidos

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class GameManager : MonoBehaviour
 {
+    public float volume = 1;
+    public Slider soundSlider;
+    public AudioMixer audioMixer;
     public static GameManager Instance { get; private set; }
     private void Awake()
     {
@@ -17,15 +22,18 @@ public class GameManager : MonoBehaviour
             Debug.Log("Warning: multiple " + this + " in scene!");
         }
     }
+    public void getVolume(float vol)
+    {
+        audioMixer.SetFloat("volume", vol);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        soundSlider = gameObject.GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
---
+++ Assets/Scripts/soundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManager : MonoBehaviour
+{
+    public GameObject obtainVolume;
+    public float volume;
+    private AudioSource audioSrc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume = obtainVolume.GetComponent<soundManager>().volume;
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        volume = obtainVolume.GetComponent<soundManager>().volume;
+        audioSrc.volume = volume;
+    }
+}
