691acd3f6bc82f02f1756e4ece74118b9a060811
Enti-Pol <pol.garriga@enti.cat>
2021-05-22 19:27:42 +0200

    Pequeños cambios

--- Assets/Scripts/clockRotation.cs
+++ Assets/Scripts/clockRotation.cs
@@ -4,14 +4,8 @@ using UnityEngine;
 
 public class clockRotation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         gameObject.transform.Rotate(0, 0, +0.4f);
     }
